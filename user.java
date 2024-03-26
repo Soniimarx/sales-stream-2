e40f2bd6f3ffe5f00fed66fc026b412f76c7e1a6
soniimarx <soniimarx@gmail.com>
2024-03-26 14:39:42 +0000

    Added methods in the user class

--- user.java
+++ user.java
@@ -3,6 +3,26 @@ class User {
     private String username;
     private String password;
     private String role; // "customer", "admin", or "employee"
+
+    public void User();
+
+    public void User(String username, String password, String role){
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public void setRole(String role){
+        this.role = role;
+    }
 }
 
 // Create a class to represent a User Login Service
